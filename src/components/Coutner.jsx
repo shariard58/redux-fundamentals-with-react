1a06426318328398001e15c2bcbef24881dea0ea
shariard58 <shariard58@gmail.com>
2024-07-31 10:52:33 +0600

    remove state and logic from coutner component

--- src/components/Coutner.jsx
+++ src/components/Coutner.jsx
@@ -1,21 +1,24 @@
 import React from "react";
-import { useState } from "react";
-export default function Coutner() {
-  const [count, setCount] = useState(0);
+
+export default function Coutner({ count, incrementCount, decrementCount }) {
   return (
-    <div class="max-w-md mx-auto mt-10 space-y-5">
-      <div class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
-        <div class="text-2xl font-semibold">{count}</div>
-        <div class="flex space-x-3">
+    <div className="max-w-md mx-auto mt-10 space-y-5">
+      <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
+        <div className="text-2xl font-semibold">{count.count}</div>
+        <div className="flex space-x-3">
           <button
-            onClick={() => setCount((prev) => prev + 1)}
-            class="bg-indigo-400 text-white px-3 py-2 rounded shadow"
+            onClick={() => {
+              incrementCount(count.id);
+            }}
+            className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
           >
             Increment
           </button>
           <button
-            onClick={() => setCount((prev) => prev - 1)}
-            class="bg-red-400 text-white px-3 py-2 rounded shadow"
+            onClick={() => {
+              decrementCount(count.id);
+            }}
+            className="bg-red-400 text-white px-3 py-2 rounded shadow"
           >
             Decrement
           </button>
