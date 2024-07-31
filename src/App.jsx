0da70e699843aa9ed80f77470527c20eacd9eb81
shariard58 <shariard58@gmail.com>
2024-07-31 10:53:10 +0600

    wrote all the llogics in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,52 @@
 import Coutner from "./components/Coutner";
+import Stats from "./components/Stats";
+import { useState } from "react";
 export default function App() {
+  const [count, setCount] = useState([
+    {
+      id: 0,
+      count: 4,
+    },
+
+    {
+      id: 1,
+      count: 5,
+    },
+  ]);
+
+  const incrementCount = (counterNumber) => {
+    setCount((prevCount) =>
+      prevCount.map((item) =>
+        item.id === counterNumber ? { ...item, count: item.count + 1 } : item
+      )
+    );
+  };
+
+  const decrementCount = (counterNumber) => {
+    setCount((prevCount) =>
+      prevCount.map((item) =>
+        item.id === counterNumber ? { ...item, count: item.count - 1 } : item
+      )
+    );
+  };
+
   return (
-    <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
-      <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
+    <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+      <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
         Simple Counter Application
       </h1>
-      <Coutner />
+      <div className="max-w-md mx-auto mt-10 spacy-y-5">
+        {count.map((singleCount) => (
+          <Coutner
+            key={singleCount.id}
+            count={singleCount}
+            incrementCount={incrementCount}
+            decrementCount={decrementCount}
+          />
+        ))}
+
+        <Stats count={count} />
+      </div>
     </div>
   );
 }
