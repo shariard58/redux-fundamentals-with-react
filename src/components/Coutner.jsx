d9fce45e84e434cf588ff813efb335ea62a4d27c
shariard58 <shariard58@gmail.com>
2024-08-01 10:08:15 +0600

    simplyfiy the coutner app as before

--- src/components/Coutner.jsx
+++ src/components/Coutner.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import { useState } from "react";
 
-export default function Coutner({ count, incrementCount, decrementCount }) {
+export default function Coutner() {
+  const [count, setCount] = useState(0);
   return (
     <div className="max-w-md mx-auto mt-10 space-y-5">
       <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
-        <div className="text-2xl font-semibold">{count.count}</div>
+        <div className="text-2xl font-semibold">{count}</div>
         <div className="flex space-x-3">
           <button
             onClick={() => {
-              incrementCount(count.id);
+              setCount((prev) => prev + 1);
             }}
             className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
           >
@@ -16,7 +18,7 @@ export default function Coutner({ count, incrementCount, decrementCount }) {
           </button>
           <button
             onClick={() => {
-              decrementCount(count.id);
+              setCount((prev) => prev - 1);
             }}
             className="bg-red-400 text-white px-3 py-2 rounded shadow"
           >
