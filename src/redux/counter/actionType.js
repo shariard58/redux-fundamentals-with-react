9741233b4ab10a13cdfc5fea2a31ae99b1782031
shariard58 <shariard58@gmail.com>
2024-08-01 09:40:40 +0600

    action type created

---
+++ src/redux/counter/actionType.js
@@ -0,0 +1,4 @@
+export const INCREMENT = "counter/increment";
+export const DECREMENT = "coutner/decreemnt";
+// ekhen sudhu incremetn or sudhu decrement dite partam. but beffer practice holo j
+// featrure er jnno ei action type setar name tao samen dbo. like this coutner/increment
