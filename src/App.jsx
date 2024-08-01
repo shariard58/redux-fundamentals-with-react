0937175b0429b1d6f72c10a834e9c403f96846fe
shariard58 <shariard58@gmail.com>
2024-08-01 10:16:03 +0600

    wrapped with provider and store provided

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
+import { Provider } from "react-redux";
 import Coutner from "./components/Coutner";
-import Stats from "./components/Stats";
-import { useState } from "react";
+import store from "./redux/store";
 export default function App() {
   return (
     <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
@@ -8,7 +8,9 @@ export default function App() {
         Simple Counter Application
       </h1>
       <div className="max-w-md mx-auto mt-10 spacy-y-5">
-        <Coutner />
+        <Provider store={store}>
+          <Coutner />
+        </Provider>
       </div>
     </div>
   );
