8a3cf895aa9506de722a765144464f9213bfc879
shariard58 <shariard58@gmail.com>
2024-08-02 08:14:25 +0600

    reaname actionsTypes

--- src/redux/dynamicCounter/actionTypes.js
+++ src/redux/dynamicCounter/actionTypes.js
@@ -1,4 +1,2 @@
-export const INCREMENT = "counter/increment";
-export const DECREMENT = "coutner/decreemnt";
-// ekhen sudhu incremetn or sudhu decrement dite partam. but beffer practice holo j
-// featrure er jnno ei action type setar name tao samen dbo. like this coutner/increment
+export const DINCREMENT = "dynamicCounter/increment";
+export const DDECREMENT = "dynamicCounter/decreemnt";
