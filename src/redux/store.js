4fb73f4b9a9bfc3d0a772019b076a56314cf537d
shariard58 <shariard58@gmail.com>
2024-08-02 20:54:14 +0600

    middleware applied

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,15 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./rootReducer";
+
+// creating our first middlewar
+const myLogger = (store) => (next) => (acation) => {
+  console.log(`Action : ${JSON.stringify(acation)}`);
+  console.log(`Before : ${JSON.stringify(store.getState())}`);
+
+  // pass actions
+  return next(acation);
+  // reducer function er kas theke action ta atkay rakha hoisilo seta k chare dawa hoilo...
+};
 const store = createStore(rootReducer, applyMiddleware(myLogger));
 
 export default store;
