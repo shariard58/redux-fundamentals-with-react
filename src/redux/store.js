e412f3d4726a2e6d401c65820a5ccb57659e4ec5
shariard58 <shariard58@gmail.com>
2024-08-02 19:53:30 +0600

    middleware concept introduced in store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./rootReducer";
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(myLogger));
 
 export default store;
