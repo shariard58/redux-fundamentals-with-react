ae3cdfbf20c59e8a319b9aa50a7550e1c2029343
shariard58 <shariard58@gmail.com>
2024-08-03 10:25:15 +0600

    applied package for redux debugger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,14 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./rootReducer";
 import myLogger from "./middlewares/myLogger";
+import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension"; // eta import kore anbo
 
-const store = createStore(rootReducer, applyMiddleware(myLogger));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(myLogger))
+);
 
 export default store;
+
+// composeWithDevTools function dia applyMiddleware function take wrap kre dibo
